1ff5cef2edde530b6f67e8b9ba8fc2765ac787ad
Diaz Linggaputra <diazlinggaputra@gmail.com>
2023-05-28 23:08:40 +0800

    feat: implement header navigation bar

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -2,10 +2,21 @@ import NewsCard from "../components/NewsCard"
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-semibold text-center mb-8">Card List</h1>
-      <NewsCard />
-    </div>
+    <>
+      <header className="bg-bone-white text-black py-4 mb-4 shadow-md">
+        <div className="container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8">
+          <a className=" text-xl font-semibold cursor-pointer">
+            News Aggregator
+          </a>
+          <button className=" py-2 px-4 transition duration-300 hover:underline font-semibold">
+            Sign In
+          </button>
+        </div>
+      </header>
+      <div>
+        <NewsCard />
+      </div>
+    </>
   );
 }
 
