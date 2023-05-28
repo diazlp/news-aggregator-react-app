b2c7a3b7be6e061d9a9249c8ab26cd5356a176bb
Diaz Linggaputra <diazlinggaputra@gmail.com>
2023-05-28 23:10:14 +0800

    feat: make header nav component abstractable

---
+++ src/components/HeaderNav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HeaderNav = () => {
+  return (
+    <header className="bg-bone-white text-black py-4 mb-4 shadow-md">
+      <div className="container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8">
+        <a className=" text-xl font-semibold cursor-pointer">
+          News Aggregator
+        </a>
+        <button className=" py-2 px-4 transition duration-300 hover:underline font-semibold">
+          Sign In
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default HeaderNav
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,21 +1,11 @@
+import HeaderNav from "../components/HeaderNav";
 import NewsCard from "../components/NewsCard"
 
 const HomePage = () => {
   return (
     <>
-      <header className="bg-bone-white text-black py-4 mb-4 shadow-md">
-        <div className="container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8">
-          <a className=" text-xl font-semibold cursor-pointer">
-            News Aggregator
-          </a>
-          <button className=" py-2 px-4 transition duration-300 hover:underline font-semibold">
-            Sign In
-          </button>
-        </div>
-      </header>
-      <div>
-        <NewsCard />
-      </div>
+      <HeaderNav />
+      <NewsCard />
     </>
   );
 }
